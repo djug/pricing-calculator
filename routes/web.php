a3ad6eeebdddfd7b16723eaf5b00859ab2cde80b
Youghourta Benali <youghourta@gmail.com>
2019-07-25 09:11:06 +0100

    add a simple demo on /demo that calculate the pricing and shows the steps

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+use App\Item;
+use App\PriceCalculator;
+
+
+Route::get('/demo', function () {
+    $item = Item::find(1);
+
+    $calculator = new PriceCalculator($item);
+
+    $price = $calculator->debugMode(true)->calculate();
+    print "Final Price: ".$price."<br>";
+
+    $steps = $calculator->getDebugStack();
+
+    dd($steps);
 });
