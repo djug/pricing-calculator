9ea8888bb58a575cd5660e37376fe9ae5251d95a
Youghourta Benali <youghourta@gmail.com>
2019-07-25 09:08:49 +0100

    add DayBasedFixedPrice, a tweaker that returned a fixed price  if the day condition is met

---
+++ app/Tweaks/DayBasedFixedPrice.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Tweaks;
+use App\Tweaks\Traits\HasTweakParameter;
+use App\Tweaks\Traits\HasTweakCondition;
+use Carbon\Carbon;
+
+/**
+ * This tweak is applicable when a day based condition
+ * is met and return a fixed price
+ */
+class DayBasedFixedPrice implements TweakInterface
+{
+    use HasTweakParameter;
+    use HasTweakCondition;
+
+    protected $today;
+
+    public function __construct()
+    {
+        $this->today = Carbon::today();
+    }
+
+    public function tweak($price)
+    {
+        $methodName = "is".ucfirst($this->condition);
+
+        if ($this->today->$methodName()) {
+            return $this->parameter;
+        }
+
+        return $price;
+    }
+}
